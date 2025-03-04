cdc0d58c447a91dd46fb5088a5ed46c402fa0df6
Hetal Patel <YOUR EMAIL ADDRESS>
2025-03-04 13:58:13 +0000

    update the code

---
+++ src/app/dm/page.js
@@ -0,0 +1,28 @@
+export default function careerPage(){
+return(
+    <>
+    <h1>Document</h1>
+    <form> 
+        <label>Name </label>
+        <input
+        type="text"
+        name="name"
+        placeholder="Full Name"
+        required
+        className="border border-gray-300 rounded-lg p-2"
+        />
+        <br />
+         <label>ID </label>
+        <input
+        type="passport"
+        name="name"
+        placeholder="Passport or Licence"
+        required
+        className="border border-gray-300 rounded-lg p-2"
+        />
+        
+    </form>
+    </>
+);
+
+}
